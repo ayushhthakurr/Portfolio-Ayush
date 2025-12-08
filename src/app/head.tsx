5ba25262a0ae8d97cc61eaf3e0bc41b019385c05
Ayush Thakur <thakurayush817@gmail.com>
2025-12-08 17:22:03 +0530

    chore: add favicon and app/head.tsx (use provided globe image)

---
+++ src/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="favicon-32x32.png" sizes="32x32" />
+      <link rel="icon" href="favicon-16x16.png" sizes="16x16" />
+      <link rel="apple-touch-icon" href="apple-touch-icon.png" />
+      <meta name="theme-color" content="#0d1117" />
+    </>
+  );
+}
